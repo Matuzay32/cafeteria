82afacd1a96a21ecd826971b6ba4b4ca65f282a0
unknown <eze.rey92@gmail.com>
2021-09-23 15:04:11 -0300

    repasando antes de aplicar docker

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({whitelist:true}))
+  app.useGlobalPipes(new ValidationPipe(
+    {
+      whitelist:true,// solamente acepta las propiedades que estan en el DTO
+      forbidNonWhitelisted:true, //esto sirve para detener el el proceso de la app para cuando se envia una propieda que no esta en la lista
+      transform: true,
+  }))
   await app.listen(3000);
 }
 bootstrap();
