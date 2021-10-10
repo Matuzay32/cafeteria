ccd4bf92b97c932743b436e7d3765ab0cf0f1361
unknown <eze.rey92@gmail.com>
2021-10-10 17:32:08 -0300

    empezando parte 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CoffesController } from './coffes/coffes.controller';
 import { CoffesService } from './coffes/coffes.service';
 import { CoffesModule } from './coffes/coffes.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports:    [CoffesModule],
+  imports:    [CoffesModule, MongooseModule.forRoot("mongodb://localhost:27017/nest-course")],
   controllers:[AppController],
   providers:  [AppService],
 })
