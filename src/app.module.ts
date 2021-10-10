a45ffb8ed7195171a3cbf3bdeb0f2f7359fcb2e6
unknown <eze.rey92@gmail.com>
2021-10-10 20:18:25 -0300

    arreglando el problema del env

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,13 +17,26 @@ import appConfig from './app.config';
 
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
+        // esto es para hacerlo con el env
+
         // el for root para traer toda la base de datos globalmente
-        type: 'postgres', // type of our database
+       /*  type: 'postgres', // type of our database
         host: process.env.DATABASE_HOST, // database host
         port: +process.env.DATABASE_PORT, // database host
         username: process.env.DATABASE_USER, // username
         password: process.env.DATABASE_PASSWORD, // user password
         database: process.env.DATABASE_NAME, // name of our database,
+        autoLoadEntities: true, // models will be loaded automatically
+        synchronize: true, // your entities will be synced with the database(recommended: disable in prod) */
+
+        //esto es para hacerlo de manera manual
+
+        type: 'postgres', // type of our database
+        host: "localhost", // database host
+        port: 5432, // database host
+        username: "postgres", // username
+        password: "matuzay32", // user password
+        database: "postgres", // name of our database,
         autoLoadEntities: true, // models will be loaded automatically
         synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
       }),
